6ca891124296e07f3eedf851e1f739e909adf44c
Tony Bowden <tony@tmtm.com>
2016-02-18 18:14:25 +0000

    Less verbosity

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,6 @@
 require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/saint-barthelemy-council', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names }, output: true)
+EveryPolitician::Wikidata.scrape_wikidata(names: { fr: names }, output: false)
 EveryPolitician::Wikidata.notify_rebuilder
 
